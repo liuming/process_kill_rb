716b1c2ef67196cf4757ce2f83e432b6ca37ab3f
Ming Liu <liuming@lmws.net>
2015-08-11 14:07:21 -0700

    draft implementation

--- lib/process_kill.rb
+++ lib/process_kill.rb
@@ -1,4 +1,59 @@
 require "process_kill/version"
 
 module ProcessKill
+
+  DEFAULT_FLOW = [
+    { signal: 'QUIT', interval: [1,2,3,5,8,13] },
+    { signal: 'TERM', max_retry: 5, interval: [1,2,3,5,8,13] },
+    { signal: 'KILL', max_retry: 3, interval: 5 },
+  ].freeze
+
+  def self.compile_flow(flow)
+    result = flow.map do |step|
+      intervals = if step[:interval].kind_of?(Array)
+        step[:max_retry] ? step[:interval].slice(0, step[:max_retry]) : step[:interval]
+      else
+        [step[:interval]] * step[:max_retry]
+      end
+      {signal: step[:signal], intervals: intervals}
+    end
+  end
+
+  def self.execute(pids, flow=DEFAULT_FLOW)
+    stats_template = flow.map{ {attempts: [], resolved: false} }
+    compiled_flow = compile_flow(flow)
+
+    result = pids.reduce({}) do |hash, pid|
+      hash.merge!(pid => stats_template.dup)
+    end
+
+    compiled_flow.each_with_index do |step, step_index|
+      step[:intervals].each do |interval|
+        pids.each do |pid|
+          begin
+            kill(step[:signal], pid) # send signal first
+            result[pid][step_index][:attempts] << interval # then increase counter
+            sleep(interval) # sleep before next interval
+          rescue ProcessNotFoundError => e
+            result[pid][step_index][:resolved] = true
+          end
+        end
+      end
+    end
+
+    result
+  end
+
+  def self.kill(signal, pid)
+    Process.kill(signal, pid)
+  rescue Errno::ESRCH => e
+    raise ProcessNotFoundError
+  end
+
+  def self.sleep(seconds)
+    Kernel.sleep(seconds)
+  end
+
+  class ProcessNotFoundError < StandardError
+  end
 end
--- spec/process_kill_spec.rb
+++ spec/process_kill_spec.rb
@@ -2,10 +2,30 @@ require 'spec_helper'
 
 describe ProcessKill do
   it 'has a version number' do
-    expect(ProcessKill::VERSION).not_to be nil
+    expect(described_class::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'has DEFAULT_FLOW' do
+    expect(described_class::DEFAULT_FLOW).not_to be nil
+  end
+
+  describe ".compile_flow" do
+    let(:flow) { [] }
+    subject {described_class.compile_flow(flow) }
+
+    context "with array of interval" do
+      let(:flow) { [{signal: 'QUIT', interval: [1,2,3]}] }
+      it { is_expected.to eq([signal: 'QUIT', intervals: [1,2,3] ])}
+    end
+
+    context "with max_retry and single interval" do
+      let(:flow) { [{signal: 'QUIT', max_retry: 3, interval: 5}] }
+      it { is_expected.to eq([signal: 'QUIT', intervals: [5,5,5] ])}
+    end
+
+    context "with an array of interval larger than max_retry" do
+      let(:flow) { [{signal: 'QUIT', max_retry: 2, interval: [1,2,3]}] }
+      it { is_expected.to eq([signal: 'QUIT', intervals: [1,2] ])}
+    end
   end
 end
