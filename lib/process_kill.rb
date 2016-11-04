65f492d20967ef5fbbb4244f54f5f49f224b62ac
Ming Liu <liuming@lmws.net>
2016-11-04 16:20:23 -0700

    refactoring and support permission error

--- lib/process_kill.rb
+++ lib/process_kill.rb
@@ -4,38 +4,54 @@ module ProcessKill
 
   DEFAULT_FLOW = [
     { signal: 'QUIT', interval: [1,2,3,5,8,13] },
-    { signal: 'TERM', max_retry: 5, interval: [1,2,3,5,8,13] },
+    { signal: 'TERM', max_retry: 5 },
     { signal: 'KILL', max_retry: 3, interval: 5 },
   ].freeze
 
   def self.compile_flow(flow)
     result = flow.map do |step|
-      intervals = if step[:interval].kind_of?(Array)
-        step[:max_retry] ? step[:interval].slice(0, step[:max_retry]) : step[:interval]
-      else
+      intervals = if step[:interval].kind_of?(Array) && step[:max_retry]
+        step[:interval].slice(0, step[:max_retry])
+      elsif step[:interval].kind_of?(Array) && !step[:max_retry]
+        step[:interval]
+      elsif step[:interval].kind_of?(Fixnum) && step[:max_retry]
         [step[:interval]] * step[:max_retry]
+      elsif step[:max_retry]
+        [1] * step[:max_retry]
+      else
+        [1]
       end
       {signal: step[:signal], intervals: intervals}
     end
   end
 
-  def self.execute(pids, flow=DEFAULT_FLOW)
-    stats_template = flow.map{ {attempts: [], resolved: false} }
-    compiled_flow = compile_flow(flow)
-
-    result = pids.reduce({}) do |hash, pid|
+  def self.generate_result_template(pids, flow)
+    stats_template = flow.map{ {attempts: [], killed: false, resolved: false} }
+    template = pids.reduce({}) do |hash, pid|
       hash.merge!(pid => stats_template.dup)
     end
 
+    return template
+  end
+
+  def self.execute(pids, flow=nil)
+    result = generate_result_template(pids, flow || DEFAULT_FLOW)
+    compiled_flow = compile_flow(flow || DEFAULT_FLOW)
     compiled_flow.each_with_index do |step, step_index|
       step[:intervals].each do |interval|
         pids.each do |pid|
+          result_item = result[pid][step_index]
+          next if result_item[:resolved]
+          result[pid][step_index][:signal] = step[:signal]
           begin
-            kill(step[:signal], pid) # send signal first
-            result[pid][step_index][:attempts] << interval # then increase counter
-            sleep(interval) # sleep before next interval
+            result[pid][step_index][:attempts] << interval # increase attempts counter
+            ProcessKill.kill(step[:signal], pid) # send signal
+            ProcessKill.sleep(interval) # sleep before next interval
           rescue ProcessNotFoundError => e
-            result[pid][step_index][:resolved] = true
+            result_item[:killed] = true
+            result_item[:resolved] = true
+          rescue UnknownError, ProcessPermissionError => e
+            result_item[:resolved] = true
           end
         end
       end
@@ -48,12 +64,14 @@ module ProcessKill
     Process.kill(signal, pid)
   rescue Errno::ESRCH => e
     raise ProcessNotFoundError
+  rescue Errno::EPERM => e
   end
 
   def self.sleep(seconds)
     Kernel.sleep(seconds)
   end
 
-  class ProcessNotFoundError < StandardError
-  end
+  class ProcessNotFoundError < StandardError; end
+  class ProcessPermissionError < StandardError; end
+  class UnknownError < StandardError; end
 end
--- spec/process_kill_spec.rb
+++ spec/process_kill_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
 describe ProcessKill do
+  before do
+    allow(described_class).to receive_messages(kill: true, sleep: true)
+  end
+
   it 'has a version number' do
     expect(described_class::VERSION).not_to be nil
   end
@@ -18,6 +22,11 @@ describe ProcessKill do
       it { is_expected.to eq([signal: 'QUIT', intervals: [1,2,3] ])}
     end
 
+    context "with max_retry and without interval" do
+      let(:flow) { [{signal: 'QUIT', max_retry: 4}] }
+      it { is_expected.to eq([signal: 'QUIT', intervals: [1,1,1,1] ])}
+    end
+
     context "with max_retry and single interval" do
       let(:flow) { [{signal: 'QUIT', max_retry: 3, interval: 5}] }
       it { is_expected.to eq([signal: 'QUIT', intervals: [5,5,5] ])}
@@ -26,6 +35,46 @@ describe ProcessKill do
     context "with an array of interval larger than max_retry" do
       let(:flow) { [{signal: 'QUIT', max_retry: 2, interval: [1,2,3]}] }
       it { is_expected.to eq([signal: 'QUIT', intervals: [1,2] ])}
+    end
+
+    context "without max_retry and interval" do
+      let(:flow) { [{signal: 'QUIT'}] }
+      it { is_expected.to eq([signal: 'QUIT', intervals: [1] ])}
+    end
+  end
+
+  describe ".generate_result_template" do
+    let(:pids) { ['pid'] }
+    let(:flow) { described_class::DEFAULT_FLOW }
+    subject { described_class.generate_result_template(pids, flow) }
+    it { is_expected.to include(*pids) }
+  end
+
+  describe ".execute" do
+    let(:pids) { ['pid'] }
+    let(:flow) { [{ signal: 'TERM', max_retry: 5 }] }
+    subject { described_class.execute(pids, flow) }
+
+    it { is_expected.to include(*pids) }
+
+    context "killed the process" do
+      before do
+        allow(described_class).to receive(:kill).and_raise(ProcessKill::ProcessNotFoundError)
+      end
+
+      it { expect(subject[pids.first]).to include({attempts: [1], killed: true, resolved: true, signal: "TERM"})}
+    end
+
+    context "failed to kill the process" do
+      it { expect(subject[pids.first]).to include({attempts: [1,1,1,1,1], killed: false , resolved: false, signal: "TERM"})}
+    end
+
+    context "has no permission to the process" do
+      before do
+        allow(described_class).to receive(:kill).and_raise(ProcessKill::ProcessPermissionError)
+      end
+
+      it { expect(subject[pids.first]).to include({attempts: [1], killed: false , resolved: true, signal: "TERM"})}
     end
   end
 end
